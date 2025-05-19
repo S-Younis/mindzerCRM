a6c37ebabc3ff0726105b91d097754ab7771d867
Younis <younish6@gmail.com>
2025-05-19 11:02:26 +0300

    installed nativewind

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,17 +1,22 @@
 import { Link } from 'expo-router';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { useTheme  } from '@react-navigation/native';
+// import { useTheme } from '@react-navigation/native';
+import { useColorScheme } from 'nativewind';
 
 export default function App() {
-   const { colors } = useTheme();
-   
+  // const { colors } = useTheme();
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+  console.log('Current color scheme:', colorScheme); // Debugging line
+
   return (
-    <View style={styles.container}  >
-      <Pressable style={{backgroundColor:'green' , width:120}} onPress={() => console.log('Hello')}>
-        <Text style={{color: colors.text }}>Change</Text>
+    <View style={styles.container}>
+      <Pressable style={{ backgroundColor: 'green', width: 120 }} onPress={() => toggleColorScheme()}>
+        <Text >Changes</Text>
       </Pressable>
-      <Text style={{ color: colors.text }}>HelloT </Text>
-      <Link style={{ color: colors.text }} href="/login">Login Page</Link>
+      <Text className="bg-red-200 dark:bg-blue-300">Branch 2 </Text>
+      <Link className='text-black dark:text-white' href="/login">
+        Login Page
+      </Link>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Slot } from 'expo-router';
+import "../global.css"
 import { ThemeProvider } from '@react-navigation/native';
 import { myDarkTheme, myLightTheme } from '../theme/theme';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -6,7 +7,6 @@ import { useColorScheme } from 'react-native';
 
 
 export default function RootLayout() {
-
    const colorScheme = useColorScheme(); // Auto-detect system color scheme
   
   return (
