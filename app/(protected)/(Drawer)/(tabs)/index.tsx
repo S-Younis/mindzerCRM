71d0997a3a6c0023e95215b9ec6ba125fef21d0c
Younis <younish6@gmail.com>
2025-06-13 00:03:54 +0300

    feat : install web ShadCn and tailwind

--- app/(protected)/(Drawer)/(tabs)/index.tsx
+++ app/(protected)/(Drawer)/(tabs)/index.tsx
@@ -3,30 +3,32 @@ import MindzerButton from '@/components/shared/MindzerButton';
 import { useState } from 'react';
 import { useAuthStore } from '@/stores/auth.store';
 import Spinner from '@/components/shared/Spinner';
+import ShadCnBTN from '@/components/web-components/ShadCnBTN';
 
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
- 
+
   const user = useAuthStore((state) => state.user);
-  return (
-    <View className="flex-1 items-center justify-center">
 
-      <Text className={`text-2xl font-bold text-blue-800 dark:text-slate-200 mb-10`}>
-        Welcome ,  {user?.name || 'Guest'}
-      </Text>
-   
+  return <ShadCnBTN />
+  // return  (
+  //   <View className="flex-1 items-center justify-center">
 
-      <MindzerButton isTitleCentered variants='primary' className='mt-4' onPress={() => {
-        setIsLoading(!isLoading);
-      }} >
-        {isLoading && <Spinner />}
-        <Text className={`font-medium  text-light`}>
-          Go To Login
-        </Text>
-      </MindzerButton>
+  //     <Text className={`text-2xl font-bold text-blue-800 dark:text-slate-200 mb-10`}>
+  //       Welcome ,  {user?.name || 'Guest'}
+  //     </Text>
+
+  //     <MindzerButton isTitleCentered variants='primary' className='mt-4' onPress={() => {
+  //       setIsLoading(!isLoading);
+  //     }} >
+  //       {isLoading && <Spinner />}
+  //       <Text className={`font-medium  text-light`}>
+  //         Go To Login
+  //       </Text>
+  //     </MindzerButton>
 
 
-    </View >
-  );
+  //   </View >
+  // );
 }
---
+++ components/web-components/ShadCnBTN.tsx
@@ -0,0 +1,14 @@
+"use dom";
+import "@/global.css";
+import { Button } from "@components/ui/button";
+
+const ShadCnBTN = () => {
+    return <div className=' h-20 w-full flex flex-col items-center justify-center'>
+        <h1 className='text-2xl font-bold mb-1 text-green-600'>ShadCn Button</h1>
+        <Button variant={"default"}  >
+            sdsadsad
+        </Button>
+    </div>
+}
+
+export default ShadCnBTN
